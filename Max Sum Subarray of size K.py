17047f35a6dee32707cbdce2cb9127932d32151b
Satyam Singh <crackcoder@outlook.com>
2024-11-26 10:43:22 +0530

    Max Sum Subarray of size K

---
+++ Max Sum Subarray of size K.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumSumSubarray (self,arr,k):
+        window_sum = sum(arr[:k])
+        max_sum = window_sum
+        
+        for i in range(k, len(arr)):
+            window_sum += arr[i] - arr[i-k]
+            max_sum = max(max_sum, window_sum)
+        return max_sum
